1d3556cccc0a1a8e7f7fc7c22c014621f0c6b0a8
John Tran <jotran@adobe.com>
2020-06-30 11:21:42 -0700

    flying sphere with particles working

--- game.ts
+++ game.ts
@@ -31,11 +31,123 @@ module GAME{
             //register resize event
             this.initWindowsResizeEvent();
 
+            //init key presses event
+            this.initKeyPressEvent();
+
         }
+
+        private initKeyPressEvent(){
+            let scene = this._scene;
+            scene.onKeyboardObservable.add((evt) => {
+                switch (evt.type) {
+                    case BABYLON.KeyboardEventTypes.KEYDOWN:          
+        
+                        switch (evt.event.key) {
+                            case "a": // passt
+                            case "A":
+                                console.log('a');
+                                break;                        
+                            case "d": // passt
+                            case "D":
+                                console.log('d');
+                                break;
+                            case "w": // passt
+                            case "W":
+                                console.log('w');
+                                break;
+                            case "s": // passt
+                            case "S":
+                                console.log('s');
+                                this.createSphere();
+                                break;                                                                              
+                         };
+                        break;
+                };
+            });
+        }
+
+        private createSphere(){
+            let scene = this._scene;
+            var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter:.5}, this._scene);
+            sphere.position.x = this.randomInt(-25, 25);
+
+            // give sphere a color
+            var myMaterial = new BABYLON.StandardMaterial("myMaterial", scene);
+            myMaterial.diffuseColor =  new BABYLON.Color3(this.randomFloatUnderOne(), this.randomFloatUnderOne(), this.randomFloatUnderOne());
+            myMaterial.specularColor = new BABYLON.Color3(this.randomFloatUnderOne(), this.randomFloatUnderOne(), this.randomFloatUnderOne());
+            myMaterial.emissiveColor = new BABYLON.Color3(this.randomFloatUnderOne(), this.randomFloatUnderOne(), this.randomFloatUnderOne());
+            myMaterial.ambientColor =  new BABYLON.Color3(this.randomFloatUnderOne(), this.randomFloatUnderOne(), this.randomFloatUnderOne());
+            sphere.material = myMaterial;
+
+            // attach particles to sphere
+            var particleSystem = new BABYLON.ParticleSystem("particles", 2000, scene);
+            //Texture of each particle
+            particleSystem.particleTexture = new BABYLON.Texture("flare.png", scene);
+
+            // Where the particles come from
+            particleSystem.emitter =sphere; //new BABYLON.Vector3(0, 0, 0); // the starting object, the emitter
+            particleSystem.minEmitBox = new BABYLON.Vector3(-0.2, -0.2, -0.2); // Starting all from
+            particleSystem.maxEmitBox = new BABYLON.Vector3(0.2, 0.2, 0.2); // To...
+
+            
+            // Size of each particle (random between...
+            particleSystem.minSize = 0.01;
+            particleSystem.maxSize = 0.1;
+
+            // Life time of each particle (random between...
+            particleSystem.minLifeTime = 0.3;
+            particleSystem.maxLifeTime = 1.5;
+
+            // Emission rate
+            particleSystem.emitRate = 2500;
+
+            // Blend mode : BLENDMODE_ONEONE, or BLENDMODE_STANDARD
+            particleSystem.blendMode = BABYLON.ParticleSystem.BLENDMODE_ONEONE;
+
+            // Set the gravity of all particles
+            particleSystem.gravity = new BABYLON.Vector3(0, -9.81, 0);
+
+            // Direction of each particle after it has been emitted
+            particleSystem.direction1 = new BABYLON.Vector3(-1, -1, 3);
+            particleSystem.direction2 = new BABYLON.Vector3(1, -1, -3);
+
+            // Angular speed, in radians
+            particleSystem.minAngularSpeed = 0;
+            particleSystem.maxAngularSpeed = Math.PI;
+
+            // Speed
+            particleSystem.minEmitPower = 1;
+            particleSystem.maxEmitPower = 3;
+            particleSystem.updateSpeed = 0.005;
+            particleSystem.start();
+
+            //make sphere move
+            var i = 0;
+            var direction = new BABYLON.Vector3(0, 1, 0);
+            direction.normalize();
+            let distance = 0.1;
+            scene.registerAfterRender(function () {
+              if(i++<200) {
+                sphere.translate(direction, distance, BABYLON.Space.WORLD);
+              }else{
+                sphere.dispose();
+              }
+            });
+
+        }
+
+        private randomInt(min: number, max: number){
+            return Math.floor(Math.random() * (max - min) ) + min;
+        }
+
+        private randomFloatUnderOne(){
+            return Math.random();
+        }
+
 
         private initCamera(){
             // Add a camera to the scene and attach it to the canvas
-            var camera = new BABYLON.UniversalCamera("UniversalCamera", new BABYLON.Vector3(0, 0, -10), this._scene);
+            var camera = new BABYLON.UniversalCamera("UniversalCamera", new BABYLON.Vector3(0, 0, -50), this._scene);
             //var camera = new BABYLON.ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, new BABYLON.Vector3(0,0,5), this._scene);
             camera.attachControl(this._canvas, true);
             return camera;
